c004c5eb339f3bb3663b6c6bdfdad20cf710748b
harukistra <sonefrree58@gmail.com>
2022-07-07 19:31:16 +0900

    [電卓を学習しました。]

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,9 +1,39 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const twiceButton = document.getElementById("twice-button")
 
 let count = 0
 
-plusButton.onclick = function () {
-  count += 1
-  display.textContent = count
+const usefulbutton = function () {
+  plusButton.onclick = function () {
+    count += 1
+    display.textContent = count
+  }
+  minusButton.onclick = function () {
+    count -= 1
+    display.textContent = count
+  }
+  twiceButton.onclick = function () {
+    count *= 2
+    display.textContent = count
+  }
+}
+
+usefulbutton()
+
+const update = function (_v) {
+  document.querySelector("input").value = _v
+}
+// const append = function (_v) {
+//   document.querySelector("input").value += _v
+// }
+const calc = function () {
+  const v = document.querySelector("input").value
+  try {
+    const f = new Function("return " + v)
+    update(f().toString()) // inputタグにあかれている文字列を実行
+  } catch (_error) {
+    update(_error)
+  }
 }
